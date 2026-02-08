da79185fc4b089d45ea357d4c8a5f71af36484ab
volodyaKNU <voloda.m@knu.ua>
2026-02-08 15:35:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Lab_1',
+  webDir: 'dist'
+};
+
+export default config;
